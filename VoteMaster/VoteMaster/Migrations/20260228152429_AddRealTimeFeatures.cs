2e3ec66a0699f53921b4072ee7325d20d52198cf
Ven <venxxx.io@gmail.com>
2026-03-01 01:20:29 +0800

    Fix .gitignore and add missing view files including Landing.cshtml

---
+++ VoteMaster/VoteMaster/Migrations/20260228152429_AddRealTimeFeatures.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoteMaster.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRealTimeFeatures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableLiveVoteCount",
+                table: "Polls",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EnablePollNotifications",
+                table: "Polls",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnableLiveVoteCount",
+                table: "Polls");
+
+            migrationBuilder.DropColumn(
+                name: "EnablePollNotifications",
+                table: "Polls");
+        }
+    }
+}
